d658a87abd7217d1a8e53478a6651524120616cd
bigpng <144656233+bigpng@users.noreply.github.com>
2025-02-14 23:11:56 +0530

    Amerifat detected opinion rejected

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Stack, Link } from 'expo-router';
-
+import { Text } from "react-native";
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
@@ -10,6 +10,9 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
+          <Text className="text-red-500 text-xl mt-4 text-right">
+              Nativewind classes are working!
+          </Text>
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
